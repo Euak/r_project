3da3dcfb691f49a703ddf02ddddfeed5c54138ca
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-10-23 17:21:23 -0300

    tratamento da tabela

--- script.R
+++ script.R
@@ -1,8 +1,25 @@
+install.packages("tidyverse", dependencies = TRUE)
+library(tidyverse)
+
 header <- scan("des_tratada.csv", nlines = 1, what = character(), sep = ",")
 header2 <- scan("des_tratada.csv", nlines = 1, what = character(), sep = ",", skip = 1)
-desocupados <- read.csv2('des_tratada.csv', sep = ",")
-names(desocupados) <- header
+desocupados <- read.csv2('des_tratada.csv', sep = ",", header = FALSE, skip = 2)
+names(desocupados) <- paste0(header, "||", header2)
 
-col.names <- c("Regiao", "Trimestre", "Período", "n")
+desocupados2 <- desocupados %>% 
+  gather("período", "quantidade", 2:131)
 
-Regiao	trimestre	periodo	n
+periodos <- str_extract(names(desocupados), "(?<=\\|\\|).*")
+
+desocupados3 <- desocupados2 %>% 
+mutate(PERIODO = periodos) 
+  
+periodos <- str_extract(desocupados2$período, "(?<=\\|\\|).*")
+
+desocupados3$período <-  str_replace(desocupados3$período, "(?<=\\|\\|).*", "")
+
+desocupados3$período <-  str_replace(desocupados3$período, "\\|\\|", "")
+
+names(desocupados)
+
+View(periodos)
