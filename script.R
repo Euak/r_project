dfcb8443c8b34dd81bd7f7057e6eabf06650e36c
Kaue Oliveira Almeida <ueak21@gmail.com>
2018-10-24 15:36:04 -0300

    exploraço de uma variavel

--- script.R
+++ script.R
@@ -1,5 +1,7 @@
 install.packages("tidyverse", dependencies = TRUE)
+install.packages("dplyr", dependencies = TRUE)
 library(tidyverse)
+library(dplyr)
 
 header <- scan("des_tratada.csv", nlines = 1, what = character(), sep = ",")
 header2 <- scan("des_tratada.csv", nlines = 1, what = character(), sep = ",", skip = 1)
@@ -35,3 +37,41 @@ desocupados4 <- desocupados3[,c(1,2,4,3)]
 desocupados_raw <- desocupados
 desocupados <- desocupados4
 rm(desocupados2,desocupados3,desocupados4)
+
+#convert value to real dimensions
+desocupados$Quantity <- desocupados$Quantity * 1000
+
+#Separating tables per region
+desocupados_estado <- subset(desocupados, !(Region %in% c("Norte", "Nordeste", "Sul", "Centro-Oeste", "Sudeste", "Brasil")))
+desocupados_regiao <- subset(desocupados, (Region %in% c("Norte", "Nordeste", "Sul", "Centro-Oeste", "Sudeste", "Brasil")))
+desocupados_brasil <- subset(desocupados, Region == "Brasil")
+
+#Remove total
+desocupados_estado <- subset(desocupados_estado, Period != "Total")
+desocupados_regiao <- subset(desocupados_regiao, Period != "Total")
+desocupados_brasil <- subset(desocupados_brasil, Period != "Total")
+
+#group by region
+desocupados_regiao %>% 
+  group_by(Region) %>% 
+  summarise(Média = mean(Quantity))
+
+desocupados_estado %>% 
+  group_by(Region) %>% 
+  summarise(Média = mean(Quantity))
+
+#group by period
+desocupados_regiao %>% 
+  group_by(Period) %>% 
+  summarise(Média = mean(Quantity))
+
+desocupados_estado %>% 
+  group_by(Period) %>% 
+  summarise(Média = mean(Quantity))
+
+#Plots
+desocupados_regiao %>% 
+  ggplot(aes(x = Period, y = Quantity)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~Region)
+  
